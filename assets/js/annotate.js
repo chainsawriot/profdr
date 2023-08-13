0d9a0baee01c901a95f621e518aeb89e42d7042f
chainsawriot <chainsawtiney@gmail.com>
2023-08-13 14:54:22 +0200

    Switch off console.log (oops)

--- assets/js/annotate.js
+++ assets/js/annotate.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', function () {
     let alinks = document.querySelectorAll(".container a");
     for (let i = 0; i < alinks.length; i++) {
 	if (alinks[i].hostname != window.location.hostname) {
-	    console.log(alinks[i].hostname);
+	    //console.log(alinks[i].hostname);
 	    let annote_node = document.createElement("span");
 	    annote_node.textContent = " [" + alinks[i].hostname + "]";
 	    annote_node.className = "post-meta";
